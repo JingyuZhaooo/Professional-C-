376435ba17c00099e8d8e1e0371f9fd1307e2291
Jingyu Zhao <jingyuz@usc.edu>
2016-09-07 02:06:06 -0700

    compression file

--- pa1/Main.cpp
+++ pa1/Main.cpp
@@ -1,6 +1,6 @@
 // Main.cpp : Defines the entry point for the console application.
 //
-
+#include <fstream>
 #include "RleTests.h"
 #include "RleData.h"
 #include <cstring>
@@ -8,6 +8,7 @@
 #include <string>
 #include <sstream>
 #include "RLEFile.h"
+
 
 void Part1Tests()
 {
@@ -24,6 +25,23 @@ int main(int argc, char* argv[])
 	else
 	{
 		// TODO: Get the file name from argv[1] and either compress/decompress
+		std::string fileName = argv[1];
+		int length = strlen(argv[1]);
+		std::cout << length << std::endl;
+		std::string suffix = ".rl1";
+		if (fileName.find(suffix) == length - 4) // found the suffix, compress file
+		{
+				
+			RleFile file;
+			file.CreateArchive(argv[1]);
+			
+		}
+		else if (fileName.find(suffix) == std::string::npos || fileName.find(suffix) != length - 4) // decompress the file
+		{
+			RleFile file;
+			file.ExtractArchive(argv[1]);
+		}
+		
 	}
 	return 0;
 }
--- pa1/RleFile.cpp
+++ pa1/RleFile.cpp
@@ -8,7 +8,55 @@
 
 void RleFile::CreateArchive(const std::string& source)
 {
-	// TODO
+	
+	std::ifstream::pos_type size;
+	char* memblock; // Open the file for input, in binary mode, and start ATE (at the end)
+	std::ifstream file(source, std::ios::in | std::ios::binary | std::ios::ate);
+	if (file.is_open())
+	{
+		size = file.tellg(); // Save the size of the file
+		memblock = new char[static_cast<unsigned int>(size)];
+		file.seekg(0, std::ios::beg); // Seek back to start of file
+		file.read(memblock, size);
+		file.close();
+		// File data has now been loaded into memblock array
+
+		mData.Compress(memblock, static_cast<size_t>(size)); // call the compress function and pass in the parameters
+			
+		// Write the header
+		mHeader.sig[0] = 'R';
+		mHeader.sig[1] = 'L';
+		mHeader.sig[2] = 'E';
+		mHeader.sig[3] = 0x01;
+		mHeader.fileSize = static_cast<int>(size);
+		mHeader.fileNameLength = static_cast<unsigned char>(source.length());
+		mHeader.fileName = source;
+		
+		std::string outFileName = const_cast<std::string&>(source) + ".rll"; // create the archieve's file name
+		// Open the file for output, in binary mode, and overwrite an existing file
+		std::ofstream arc(outFileName, std::ios::out | std::ios::binary | std::ios::trunc);
+		if (arc.is_open())
+		{
+			// Use arc.write function to write data here // where to use c_str??
+			arc.write(mHeader.sig, 4);
+			arc.write(reinterpret_cast<char*>(&(mHeader.fileSize)), 4);
+			arc.write(reinterpret_cast<char*>(&(mHeader.fileNameLength)), 1);
+			arc.write(reinterpret_cast<char*>(&(mHeader.fileName)), source.length());
+			arc.write(mData.mData, mData.mSize);
+			arc.close();
+
+			int comptressedSize = mData.mSize;
+			int	percent = 100 * (static_cast<int>(size) - comptressedSize) / static_cast<int>(size); // calculate the compression percentage
+			std::cout << percent << "% compression was achieved!" << std::endl; // only print if the file is actually open
+		}
+
+		// Make sure to clean up!
+		delete[] memblock;
+	}
+	else
+	{
+		std::cout << "Cannot open file!" << std::endl;
+	}
 }
 
 void RleFile::ExtractArchive(const std::string& source)
